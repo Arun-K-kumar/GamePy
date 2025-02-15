3e60f32bfabbc1ca7e8b663d198b7895ac854574
21stCenturyCoder <arnkmrsll@gmail.com>
2025-02-15 21:59:49 +0530

    Games
    Entertainment

---
+++ hangman.py
@@ -0,0 +1,83 @@
+def hangman():
+       from random import choice
+       #Enter any word you like
+       worlit=['god','developer','dinosaur','vivo','school','king','queen','speed']
+       a=choice(worlit)
+       blank=[]
+       word=[]
+       count=1
+       for letter in a:
+              word.append(f'{letter}')
+       for _ in range(len(word)):
+              blank.append("_")
+       while True:
+              for l in blank:
+                     print(l,end="")
+              print()             
+              w=input("Enter a letter :")
+              k=0
+              for i in word:
+                     if i==w:
+                            blank[k]=word[k]
+                     k=k+1    
+              count=count+1
+              print("Attempts :",count)
+              print()
+              p=0
+              for s in blank:
+                     if  s != '_' :
+                            p=p+1
+              if p==(len(word)):
+                     for l in blank:
+                            print(l,end="")
+                     print()             
+                     print("*** You Won ***")
+                     break
+
+
+hangman()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ stone.py
@@ -0,0 +1,42 @@
+def sps():
+    from random import choice
+
+    def line():
+        for _ in range(1):
+            for _ in range(115):
+                print("-",end="")
+        print("\n")
+    while True:
+        list=["Stone","Paper","Scissor"]
+        display=choice(list)
+        print("1. Stone")
+        print("2. Paper")
+        print("3. Scissor")
+        print("0. Exit")
+        print("Enter your Choice :",end="")
+        a=int(input())
+        print()
+        if a==1:
+            print("Player :Stone")
+        elif a==2:
+            print("Player :Paper")
+        elif a==3:
+            print("Player :Scissor")
+        elif a==0:
+            print("See you  Again")
+            break
+        elif ValueError:
+            print("Incorrect input you dick in a mouth bitch ")
+        else:
+            print("Invalid Input")   
+        print( "PC     :"+display)  
+        if (a==1 and display=="Scissor") or (a==1 and display=="Stone") or (a==3 and display=="Paper"):
+            print("***|  You Win  |***")
+        elif (a==1 and display=="Stone") or (a==2 and display=="Paper") or (a==3 and display=="Scissor"):
+            print("***|  This is a Draw  |***")
+        else:
+            print("***|  You Lose  |***")
+        line()
+        
+
+sps()
